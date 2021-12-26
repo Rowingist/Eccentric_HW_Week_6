bfac78763febc6c1d5dca6c90df65b713da33fb4
Валентин Кравченко <Rowingist@mail.ru>
2021-12-27 01:49:10 +0300

    I have added particles for bullets and gun; made Spawn() in BulletSpawner.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,8 +5,13 @@ public class Bullet : MonoBehaviour
     [SerializeField] private GameObject _effectPrefab;
     [SerializeField] private MovableAudioClip _hitSound;
 
+    private Renderer _renderer;
+    private Rigidbody _rigidbody;
+
     private void Start()
     {
+        _renderer = GetComponent<Renderer>();
+        _rigidbody = GetComponent<Rigidbody>();
         Destroy(gameObject, 4f);
     }
 
@@ -14,6 +19,8 @@ public class Bullet : MonoBehaviour
     {
         GameObject newFlash = Instantiate(_effectPrefab, transform.position, Quaternion.identity);
         newFlash.GetComponent<SelfDestruct>().Init(_hitSound.AudioClip);
-        Destroy(gameObject);
+        _renderer.enabled = false;
+        _rigidbody.isKinematic = true;
+        Destroy(gameObject, 0.5f);
     }
 }
--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -11,14 +11,21 @@ public class BulletSpawner : MonoBehaviour
     [SerializeField] private AudioSource _shootSound;
     [SerializeField] private GameObject _flash;
     [SerializeField] private Sound _hitSounds;
+    [SerializeField] private ParticleSystem _smokeTrail;
 
     private float _elapsedTime;
 
     private void Update()
     {
         _elapsedTime += Time.deltaTime;
+        Spawn();
+    }
+
+    private void Spawn()
+    {
         if (_elapsedTime > _shootPeriod && Input.GetMouseButton(0))
         {
+            _smokeTrail.Play();
             _elapsedTime = 0;
             Bullet newBullet = Instantiate(_bulletTemplate, _spawnPoint.position, _spawnPoint.rotation);
             newBullet.gameObject.GetComponent<Rigidbody>().velocity = _spawnPoint.forward * _bulletSpeed;
